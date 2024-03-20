91a00b2bfa8568bc61a630848a6029e388e41ce6
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-21 00:41:24 +0800

    Valid the logout button @ProfileViewController, and fetch APIs for displaying the user name on the profile.

--- FirebaseDemo/Controller/ProfileViewController.swift
+++ FirebaseDemo/Controller/ProfileViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
 
@@ -15,11 +17,38 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
 
         self.title = "My Profile"
+        
+        // We fetch the username data form API
+        if let currentUser = Auth.auth().currentUser {
+            nameLabel.text = currentUser.displayName
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logout(sender: UIButton) {
+        
+        do {
+            try Auth.auth().signOut()
+            
+        } catch {
+            let alertController = UIAlertController(title: "Logout Error", message: error.localizedDescription, preferredStyle: .alert)
+            let okayaction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayaction)
+            present(alertController, animated: true, completion: nil)
+            
+            return
+        }
+            
+        // Back to the welcome view, logout button segues to welcome view valid the IBAction
+        if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeView") {
+            UIApplication.shared.keyWindow?.rootViewController = viewController
+            self.dismiss(animated: true, completion: nil)
+        }
+        
     }
     
     @IBAction func close(sender: AnyObject) {
